c151289917951fb9eeb98b0c398220a135a5b5bd
PomaPoma <PomaPoma@localhost>
2018-12-01 14:27:58 +0300

    clean

--- src/main/java/ru/itpark/BonusService.java
+++ src/main/java/ru/itpark/BonusService.java
@@ -7,16 +7,19 @@ public class BonusService {
         int bonusSilverCard = 70;
         int bonusGoldCard = 100;
 
-        if (accumulatedAmount == 0){
-            result = 0;
-        } else if (accumulatedAmount >= 1 && accumulatedAmount <= 15_000) {
-            result = purchaseAmount * bonusBlueCard / 1_000;
-        } else if (accumulatedAmount >= 15_001 && accumulatedAmount <= 150_000) {
-            result = purchaseAmount * bonusSilverCard / 1_000;
-        } else {
-            result = purchaseAmount * bonusGoldCard / 1_000;
+        if (accumulatedAmount <= 0) {
+            return 0;
         }
-        // Ctrl + Alt + L - выравнивание
-        return result;
+
+        if (accumulatedAmount <= 15_000) {
+            return purchaseAmount * bonusBlueCard / 1_000;
+        }
+
+        if (accumulatedAmount <= 150_000) {
+            return purchaseAmount * bonusSilverCard / 1_000;
+        }
+
+        return purchaseAmount * bonusGoldCard / 1_000;
+
     }
 }
